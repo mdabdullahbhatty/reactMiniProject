328c793b592a5e449c8db60cc4303f812503b38b
Md Abdullah Bhatty <md016112345@gmail.com>
2025-04-29 20:07:21 +0600

    first commit

---
+++ src/About.jsx
@@ -0,0 +1,15 @@
+
+
+
+function About() {
+
+  return(
+    <div>
+      <h1>Abut Compoo</h1>
+    </div>
+  )
+  
+}
+
+    
+export default About
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router'
+import About from './About.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App/>} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
+  </BrowserRouter>,
+)
